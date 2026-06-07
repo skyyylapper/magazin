a6ff38dd83222e423afd24a79ed1a5a7892d5ba6
skyyylapper <izzqwar@mail.ru>
2026-06-07 23:56:59 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MAIN_BOT_TOKEN = os.getenv("MAIN_BOT_TOKEN")
+ADMIN_BOT_TOKEN = os.getenv("ADMIN_BOT_TOKEN")
+SEND_API_KEY = os.getenv("SEND_API_KEY")
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(',')))
+ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "")
+MANUAL_WALLET_ADDRESS = os.getenv("MANUAL_WALLET_ADDRESS", "")
+CURRENCY_UPDATE_INTERVAL = int(os.getenv("CURRENCY_UPDATE_INTERVAL", 1800))
+DEFAULT_CURRENCY = os.getenv("DEFAULT_CURRENCY", "USDT")
+ANTISPAM_MAX_INVOICES = int(os.getenv("ANTISPAM_MAX_INVOICES", 3))
+ANTISPAM_TIMEFRAME = int(os.getenv("ANTISPAM_TIMEFRAME", 600))
+ANTISPAM_BLOCK_DURATION = int(os.getenv("ANTISPAM_BLOCK_DURATION", 3600))
+DEFAULT_LANGUAGE = os.getenv("DEFAULT_LANGUAGE", "ru")
