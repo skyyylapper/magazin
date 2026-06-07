22a7458a2eb483f27bcf524e79478da715cdf32d
skyyylapper <izzqwar@mail.ru>
2026-06-08 01:31:33 +0300

    Update main_menu.py

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -8,19 +8,16 @@ def main_menu_keyboard(lang='ru'):
         btn_support = "🆘 Поддержка"
         btn_cart = "🛒 Корзина"
         btn_profile = "👤 Профиль"
-        btn_referral = "👥 Рефералы"
     else:
         btn_catalog = "🛍 Catalog"
         btn_balance = "💰 Balance"
         btn_support = "🆘 Support"
         btn_cart = "🛒 Cart"
         btn_profile = "👤 Profile"
-        btn_referral = "👥 Referrals"
     return InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text=btn_catalog, callback_data="catalog"),
          InlineKeyboardButton(text=btn_cart, callback_data="cart")],
         [InlineKeyboardButton(text=btn_balance, callback_data="balance"),
          InlineKeyboardButton(text=btn_profile, callback_data="profile")],
-        [InlineKeyboardButton(text=btn_referral, callback_data="referral"),
-         InlineKeyboardButton(text=btn_support, url=f"https://t.me/{ADMIN_USERNAME}")]
+        [InlineKeyboardButton(text=btn_support, url=f"https://t.me/{ADMIN_USERNAME}")]
     ])
